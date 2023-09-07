e0d0f004504484f7147a8ca47fcad5e33afcdf51
PrerakPithadiya <139850989+PrerakPithadiya@users.noreply.github.com>
2023-09-07 10:09:17 +0530

    Update Pattern_5.java

--- PePCoding/Basics/Pattern_5.java
+++ PePCoding/Basics/Pattern_5.java
@@ -1,3 +1,15 @@
+/*
+
+      * 
+    * * * 
+  * * * * * 
+* * * * * * * 
+  * * * * * 
+    * * * 
+      * 
+
+*/
+
 import java.util.Scanner;
 public class Pattern_5{
     public static void main(String[] args){
