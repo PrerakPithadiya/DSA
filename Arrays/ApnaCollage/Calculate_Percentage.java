b91582e3ffe68db2d46c79534ec98f3d0928c484
PrerakPithadiya <139850989+PrerakPithadiya@users.noreply.github.com>
2023-09-07 15:31:16 +0530

    Add files via upload

---
+++ Arrays/ApnaCollage/Calculate_Percentage.java
@@ -0,0 +1,9 @@
+public class Calculate_Percentage{
+    public static void main(String[] args){
+        int marks[] = {12, 23, 34, 45};
+
+        float percentage = (marks[0] + marks[1] + marks[2] + marks[3]) / 4.0f;
+
+        System.out.println("The Percentage of Values is : " + percentage);
+    }
+}
