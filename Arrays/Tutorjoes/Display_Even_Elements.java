118e462423f2df783b3991cdda3e8ddd74a82b0d
PrerakPithadiya <139850989+PrerakPithadiya@users.noreply.github.com>
2023-09-09 22:20:01 +0530

    Add files via upload

---
+++ Arrays/Tutorjoes/Display_Even_Elements.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+import java.util.Random;
+import java.util.Arrays;
+import java.util.List;
+import java.util.ArrayList;
+public class Display_Even_Elements{
+    public static void generateArray(int arr[]){
+        Random rd = new Random();
+        int n = arr.length;
+        for(int i = 0; i < n; i++){
+            arr[i] = rd.nextInt(0, 100);
+        }
+    }
+    public static List<Integer> printEvens(int arr[]){
+        List<Integer> list = new ArrayList<>();
+        for(int element : arr){
+            if(element % 2 == 0){
+                list.add(element);
+            }
+        }
+        return list;
+    }
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter Array Size : ");
+        int size = sc.nextInt();
+        int arr[] = new int[size];
+        generateArray(arr);
+        System.out.println("Array -> " + Arrays.toString(arr));
+        System.out.println("Even Elements -> " + printEvens(arr));
+    }
+}
