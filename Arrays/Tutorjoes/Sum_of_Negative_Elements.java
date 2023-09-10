644e1678ab963e66ed73874df5c354fd26a858cf
PrerakPithadiya <139850989+PrerakPithadiya@users.noreply.github.com>
2023-09-10 16:33:23 +0530

    Add files via upload

---
+++ Arrays/Tutorjoes/Sum_of_Negative_Elements.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+import java.util.Random;
+import java.util.Arrays;
+public class Sum_of_Negative_Elements{
+    public static void generateArray(int arr[]){
+        Random rd = new Random();
+        int n = arr.length;
+        for(int i = 0; i < n; i++){
+            arr[i] = rd.nextInt(-100, 100);
+        }
+    }
+    public static int sumOfNegatives(int arr[]){
+        int sum = 0;
+        for(int element : arr){
+            if(element < 0){
+                sum = sum + element;
+            }
+        }
+        return sum;
+    }
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter Array Size : ");
+        int size = sc.nextInt();
+        int arr[] = new int[size];
+        generateArray(arr);
+        System.out.println("Array -> " + Arrays.toString(arr));
+        System.out.println("Sum of Negative Elements -> " + sumOfNegatives(arr));
+    }
+}
