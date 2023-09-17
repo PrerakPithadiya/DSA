2e300b16962f28547975554030b5283b4213e6f1
PrerakPithadiya <139850989+PrerakPithadiya@users.noreply.github.com>
2023-09-17 22:51:40 +0530

    Add files via upload

--- Arrays/LeetCode/Find_Numbers_with_Even_Number_of_Digits.java
+++ Arrays/LeetCode/Find_Numbers_with_Even_Number_of_Digits.java
@@ -1,1 +1,20 @@
-
+import java.lang.StringBuilder;
+public class Find_Numbers_with_Even_Number_of_Digits{
+    public static void main(String[] args){
+        int arr[] = {555, 901, 482, 1771};
+        Find_Numbers_with_Even_Number_of_Digits cN = new Find_Numbers_with_Even_Number_of_Digits();
+        System.out.println(cN.findNumbers(arr));
+    }
+    public int findNumbers(int[] nums) {
+        // StringBuilder sb = new StringBuilder();
+        int count = 0;
+        for(int element : nums){
+            // sb.append(element);
+            if(((int)(Math.log10(element)) + 1) % 2 == 0){
+                count++;
+            }
+            // sb.setLength(0);
+        }
+        return count;
+    }
+}
