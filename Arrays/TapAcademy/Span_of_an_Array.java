acd09464fbad1fccd65cf64be6d172d82f7ba848
PrerakPithadiya <139850989+PrerakPithadiya@users.noreply.github.com>
2023-09-21 19:36:51 +0530

    Add files via upload

--- Arrays/TapAcademy/Span_of_an_Array.java
+++ Arrays/TapAcademy/Span_of_an_Array.java
@@ -1,1 +1,24 @@
-
+import java.util.Arrays;
+public class Span_of_an_Array{
+    public static void main(String[] args){
+        int arr[] = {20, 42, 88, 10, 99, 6};
+        int span = arraySpan(arr);
+        System.out.println("Span of an Array : " + span);
+    }
+    public static int arraySpan(int arr[]){
+        int max = Integer.MIN_VALUE, min = Integer.MAX_VALUE;
+        for(int element : arr){
+            if(max < element){
+                max = element;
+            } if(min > element){
+                min = element;
+            }
+        }
+        return max - min;
+    }
+    public static int arraySpan2(int arr[]){
+        Arrays.sort(arr);
+        int min = arr[0], max = arr[arr.length - 1];
+        return max - min;
+    }
+}
