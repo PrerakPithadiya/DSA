11740b9ccd579684902d48d21b907f86fc3a6b7b
PrerakPithadiya <139850989+PrerakPithadiya@users.noreply.github.com>
2023-10-24 14:03:48 +0530

    Add files via upload

--- Strings/LeetCode/Goal_Parser_Interpretation.java
+++ Strings/LeetCode/Goal_Parser_Interpretation.java
@@ -1,1 +1,26 @@
-
+import java.lang.StringBuilder;
+public class Goal_Parser_Interpretation{
+    public static void main(String[] args){
+        String str = "(al)G(al)()()G";
+        System.out.println(interpret(str));
+    }
+    public static String interpret(String command) {
+        StringBuilder sb = new StringBuilder();
+        int n = command.length();
+        for(int i = 0; i < n; i++){
+            char ch = command.charAt(i);
+            if(ch == 'G'){
+                sb.append("G");
+            } else{
+                if(command.charAt(i + 1) == ')'){
+                    sb.append("o");
+                    i++;
+                } else{
+                    sb.append("al");
+                    i += 3;
+                }
+            }
+        }
+        return sb.toString();
+    }
+}
