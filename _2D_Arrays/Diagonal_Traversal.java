874868f1d6a3ed49579f074babb3ff9b924b1dc6
PrerakPithadiya <139850989+PrerakPithadiya@users.noreply.github.com>
2024-01-29 03:58:17 -0800

    Add files via upload

---
+++ _2D_Arrays/Diagonal_Traversal.java
@@ -0,0 +1,48 @@
+package _2D_Arrays;
+
+import java.util.Arrays;
+
+public class Diagonal_Traversal {
+    public static void main(String[] args) {
+        int[][] mat = {
+                {1, 2, 3},
+                {4, 5, 6},
+                {7, 8, 9}
+        };
+        int[] result = diagonalTraversal(mat);
+        System.out.println(Arrays.toString(result));
+    }
+
+    public static int[] diagonalTraversal(int[][] mat) {
+        int r = mat.length, c = mat[0].length;
+        int diagonalNo = 1, row = 0, col = 0, k = 0;
+        int[] arr = new int[r * c];
+        while (row < r && col < c) {
+            int i = row;
+            int j = col;
+            if (diagonalNo % 2 != 0) {
+                for (; i > -1 && j < c; i--, j++) {
+                    arr[k++] = mat[i][j];
+                }
+                row = i + 1;
+                col = j - 1;
+                if (col == c - 1)
+                    row++;
+                else
+                    col++;
+            } else {
+                for (; i < r && j > -1; i++, j--) {
+                    arr[k++] = mat[i][j];
+                }
+                row = i - 1;
+                col = j + 1;
+                if (row == r - 1)
+                    col++;
+                else
+                    row++;
+            }
+            diagonalNo++;
+        }
+        return arr;
+    }
+}
